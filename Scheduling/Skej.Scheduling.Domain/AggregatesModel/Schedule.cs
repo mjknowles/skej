522354a00e4cdae26bc7998968968841f075c058
mjknowles <mjknowles23@gmail.com>
2017-09-06 21:50:48 -0500

    Begin adding Schedule domain model.

---
+++ Scheduling/Skej.Scheduling.Domain/AggregatesModel/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using Skej.Scheduling.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skej.Scheduling.Domain.AggregatesModel
+{
+    public class Schedule : Entity, IAggregateRoot
+    {
+        // copy from https://github.com/mlipper/runt/blob/master/lib/runt/schedule.rb
+    }
+}
---
+++ Scheduling/Skej.Scheduling.Domain/SeedWork/IAggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skej.Scheduling.Domain.SeedWork
+{
+    public interface IAggregateRoot { }
+}
