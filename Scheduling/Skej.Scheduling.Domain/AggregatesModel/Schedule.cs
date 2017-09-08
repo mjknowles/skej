c22fb07efba4c07e5760a95b971a33e2c326c02e
mjknowles <mjknowles23@gmail.com>
2017-09-07 23:43:46 -0500

    Added TemporalExpressions.

--- Scheduling/Skej.Scheduling.Domain/AggregatesModel/Schedule.cs
+++ Scheduling/Skej.Scheduling.Domain/AggregatesModel/Schedule.cs
@@ -8,5 +8,61 @@ namespace Skej.Scheduling.Domain.AggregatesModel
     public class Schedule : Entity, IAggregateRoot
     {
         // copy from https://github.com/mlipper/runt/blob/master/lib/runt/schedule.rb
+        private HashSet<ScheduleElement> _elements;
+
+        public void Add(ScheduleElement element) => _elements.Add(element);
+
+        public bool IsOccuring(string eventArg, DateTime aDate)
+        {
+            foreach(var e in _elements)
+            {
+                if (e.IsOccuring(eventArg, aDate)) return true;
+            }
+            return false;
+        }
     }
+
+    public class ScheduleElement
+    {
+        public string Event { get; }
+        public TemporalExpression Expression { get; }
+
+        public ScheduleElement(string eventArg, TemporalExpression temporalExpression)
+        {
+            Event = eventArg;
+            Expression = temporalExpression;
+        }
+
+        public bool IsOccuring(string eventArg, DateTime aDate) => Event == eventArg ? Expression.Includes(aDate) : false;
+        
+    }
+    
+    public abstract class TemporalExpression
+    {
+        public abstract bool Includes(DateTime aDate);
+    }
+
+    /// <summary>
+    /// Handles scenario such as "first monday of every month"
+    /// </summary>
+    public class DayInMonth : TemporalExpression
+    {
+        private int _count;
+        private int _dayIndex;
+
+        public DayInMonth(int dayIndex, int count)
+        {
+            _dayIndex = dayIndex;
+            _count = count;
+        }
+
+        public override bool Includes(DateTime aDate) =>  DayMatches(aDate) && WeekMatches(aDate);
+        private bool DayMatches(DateTime aDate) => aDate.Day == _dayIndex;
+        private bool WeekMatches(DateTime aDate) => _count > 0 ? WeekFromStartMatches(aDate) : WeekFromEndMatches(aDate);
+        private bool WeekFromStartMatches(DateTime aDate) => WeekInMonth(aDate.Day) == _count;
+        private bool WeekFromEndMatches(DateTime aDate) => WeekInMonth(DaysLeftInMonth(aDate) + 1) == Math.Abs(_count);
+        private int WeekInMonth(int dayNumber) => ((dayNumber - 1) / 7) + 1;
+        private int DaysLeftInMonth(DateTime aDate) => DateTime.DaysInMonth(aDate.Year, aDate.Month) - aDate.Day;
+    }
+    
 }
