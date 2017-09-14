3b6c81363bb444351cdaca96ca0da3e1ce2363b6
mjknowles <mjknowles23@gmail.com>
2017-09-13 23:41:01 -0500

    Beginning adding CreateScheduleCommandHandler.

--- src/Services/Scheduling/Skej.Scheduling.API/Application/CreateScheduleElementCommandHandler.cs
+++ src/Services/Scheduling/Skej.Scheduling.API/Application/CreateScheduleElementCommandHandler.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using MediatR;
+using Scheduling.Infrastructure.Idempotency;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Scheduling.API.Application
 {
-    public class CreateScheduleElementCommandHandler
+    public class CreateScheduleElementCommandHandler : IdentifierCommandHandler<CreateScheduleElementCommand, bool>
     {
+        public CreateScheduleElementCommandHandler(IMediator mediator, IRequestManager requestManager) 
+            : base(mediator, requestManager)
+        {
+        }
+
+        protected override bool CreateResultForDuplicateRequest()
+        {
+            return true;                // Ignore duplicate requests for creating order.
+        }
     }
 }
